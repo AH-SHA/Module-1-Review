f876a500fcd2ade04dd308b228286300ae5032d8
Shannon Ashford <email.sa.coder@gmail.com>
2025-02-11 23:08:21 -0500

    Initial Commit. Code for Area of a Circle

--- obj/Debug/net8.0/Module 1 Review Exercise - C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/Module 1 Review Exercise - C#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Module 1 Review Exercise - C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfac28340dfc1a633a1150b6736bdd63b899d109")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ceb00a0b7146aa0f5cef27e8ebf838cd005fd6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Module 1 Review Exercise - C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Module 1 Review Exercise - C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
