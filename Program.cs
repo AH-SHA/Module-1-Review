4747f6daaed5fcb7082bc2904fb84e139f420cea
Shannon Ashford <email.sa.coder@gmail.com>
2025-02-12 00:16:19 -0500

    Added Method calcs for a Triangle's area

--- Program.cs
+++ Program.cs
@@ -5,10 +5,14 @@
     {
         static void Main(string[] args)
         {
-         // AREA of a CIRCLE
+            // AREA of a CIRCLE
+
+            Console.WriteLine("AREA of a CIRCLE");
+            Console.WriteLine();
+
             // Code to ask the user to provide input
 
-            Console.WriteLine("Please type a number on the next line for the radius: ");
+            Console.WriteLine("Please type a number on the next line for the circle's radius: ");
 
             //Create a variable called 'radius'
             //Converting user input to the 'double' variable type
@@ -17,19 +21,53 @@
 
             //Calling the method 'CircleArea' and printing the method's result to the console
 
-            Console.WriteLine(CircleArea(radius));
+            Console.WriteLine($"The area of your circle is: {CircleArea(radius)}");
 
-         // AREA of a TRIANGLE
-
-
-
-
-         //AREA of a RECTANGLE
+            Console.WriteLine();
 
 
 
 
-         // AREA of a SQUARE
+          // AREA of a TRIANGLE
+
+            Console.WriteLine("AREA of a TRIANGLE");
+
+            Console.WriteLine("Enter a number on the next line for the triangle's BASE: ");
+
+
+            double baseLength = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Enter a number on the next line for the triangle's HEIGHT: ");
+
+
+            double height = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"The area of your triangle is: {TriangleArea(baseLength, height)}");
+
+            Console.WriteLine();
+
+
+            //AREA of a RECTANGLE
+
+
+
+            Console.WriteLine("AREA of a RECTANGLE");
+
+
+
+
+
+            Console.WriteLine();
+
+            // AREA of a SQUARE
+            Console.WriteLine("AREA of a SQUARE");
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+
 
 
         }
@@ -47,7 +85,14 @@
 
         // Creating the method 'TriangleArea' within the class of the project
 
+        static double TriangleArea(double baseLength, double height)
+        {
+            //Codeblock for the TriangleArea method to calculate area
+            //TriangleArea method uses 2 parameters: baseLength and height
 
+            return (baseLength / 2) * height;
+
+        }
 
 
     }
